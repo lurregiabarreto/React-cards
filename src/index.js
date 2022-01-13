1d0573aecdec8c461da571477118f75f88fa3861
Lurrégia Barreto <luhhbarreto@gmail.com>
2022-01-13 16:04:45 -0300

    renderizar

--- src/index.js
+++ src/index.js
@@ -2,8 +2,24 @@ import './index.css'
 import React from 'react'
 import ReactDOM from 'react-dom'
 import Primeiro from './Components/Primeiro'
+import ComParametro from'./Components.ComParametro'
+import ComFilhos from './Components/ComFilhos'
+
+
 
 ReactDOM.render(
-   <h1> Olá React!</h1>,
-   document.getElementById('root') 
+    <div>
+        <ComFilhos>
+            <ul>
+                <li> Ana </li>
+                <li> Bia </li>
+                <li> Carlos </li>
+                <li> Daniel  </li>
+            </ul>
+        </ComFilhos>
+        <Primeiro></Primeiro>
+   <ComParametro titulo = "Esse é o titulo" subtitulo = "Esse é o subtitulo"></ComParametro>
+    </div>
+   
+   ,document.getElementById('root') 
 )
