0b26c67ecd4f6ec808c364c53d9ffb93d28952aa
Lurrégia Barreto <luhhbarreto@gmail.com>
2022-01-13 17:49:06 -0300

    adicionar o app

--- src/index.js
+++ src/index.js
@@ -1,27 +1,10 @@
 import './index.css'
-import React from 'react'
-import ReactDOM from 'react-dom'
-import Primeiro from './Components/Primeiro'
-import ComParametro from'./Components.ComParametro'
-import ComFilhos from './Components/ComFilhos'
-import Card from '.Componentes/layout/Card'
+import React from "react"
+import ReactDom from "react-dom"
+import App from './App.jsx'
 
 
-
-ReactDOM.render(
-    <div>
-        <Card titulo = "Exercicio X"> Conteudo </Card>
-        <ComFilhos>
-            <ul>
-                <li> Ana </li>
-                <li> Bia </li>
-                <li> Carlos </li>
-                <li> Daniel  </li>
-            </ul>
-        </ComFilhos>
-        <Primeiro></Primeiro>
-   <ComParametro titulo = "Esse é o titulo" subtitulo = "Esse é o subtitulo"></ComParametro>
-    </div>
-   
-   ,document.getElementById('root') 
+ReactDom.render(
+<App></App>,
+  document.getElementById('root')
 )
