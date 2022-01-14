d56c63c004242fa3a565497864aeb8c62fd5fe2f
Lurrégia Barreto <luhhbarreto@gmail.com>
2022-01-14 11:08:51 -0300

    alterar para props

--- src/Components/ComParametro.jsx
+++ src/Components/ComParametro.jsx
@@ -1,9 +1,13 @@
-import React from 'react' 
+import React from "react";
 
 // eslint-disable-next-line import/no-anonymous-default-export
-export default param =>
+export default (props) => {
 
-<>
-    <h3>{param.titulo}</h3>
-    <p>{param.subtitulo}</p>
-</>
+    return (
+        <>
+            <h3>{props.titulo}</h3>
+            <p>{props.subtitulo}</p>
+        </>
+
+    );
+}
